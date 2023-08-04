34a9a986f36e635737f66410a7a8c7dbf3e89451
陈永超 <chen_yongchao@126.com>
2023-08-04 18:57:11 +0800

    microapp引入

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
+/** microApp */
+import microApp from "@micro-zoe/micro-app";
+
+microApp.start({});
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,6 +4,12 @@ function page404() {
   return (
     <h2>
       首页
+      <micro-app 
+        iframe
+        name='my-app' 
+        url='https://microapp.lammu.cn/microappA/'
+        default="/microappA/a"
+      ></micro-app>
     </h2>
   )
 }
