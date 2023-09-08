ea79849fca50d7a888561b254e52a671e57eb29c
LAMMU <chen_yongchao@126.com>
2023-09-09 03:00:58 +0800

    路由面包屑

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Footer from './layouts/Footer';
 import lowcode from './pages/lowcode';
 import 'antd/dist/antd.css'
 import '@/style/index.scss'
+import { Breadcrumb } from 'antd';
 
 function App() {
   return (
@@ -18,27 +19,36 @@ function App() {
           <section
             style={{
               flex: '1',
-              padding: '8px 10px',
+              padding: '0 10px 8px',
               backgroundColor: '#e2e2e2',
               overflow: 'scroll',
               display: 'flex',
               flexDirection: 'column'
             }}
           >
+            <Breadcrumb>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <a href="">Application Center</a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>
+                <a href="">Application List</a>
+              </Breadcrumb.Item>
+              <Breadcrumb.Item>An Application</Breadcrumb.Item>
+            </Breadcrumb>
             <div
               style={{
                 flex: 1,
                 backgroundColor: 'white',
               }}
             >
-              
-                <Switch>
-                  <Route path='/lowcode' component={lowcode}></Route>
-                  <Route path='/404' component={page404}></Route>
-                  <Route path='/' component={home}></Route>
-                  <Redirect from="/index" to="/"/>
-                  <Route component={ page404 } />
-                </Switch>
+              <Switch>
+                <Route path='/lowcode' component={lowcode}></Route>
+                <Route path='/404' component={page404}></Route>
+                <Route path='/' component={home}></Route>
+                <Redirect from="/index" to="/"/>
+                <Route component={ page404 } />
+              </Switch>
               {/* <Footer></Footer> */}
             </div>
           </section>
