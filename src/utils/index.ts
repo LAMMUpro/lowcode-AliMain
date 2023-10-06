1b986d41ff2a6435506ed7649ed18f8c04ab39da
LAMMU <chen_yongchao@126.com>
2023-10-06 19:01:20 +0800

    防抖截流函数

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -68,3 +68,39 @@ export function parseLocalInt(value: string|null):number {
   if (value) return +value;
   return '' as unknown as number;
 }
+
+/**
+ * 防抖函数
+ * @param fn 执行函数
+ * @param delay 延时（ms）
+ * @returns 防抖后的函数
+ */
+export function debounce(fn: Function, delay: number = 500) {
+  let timer: number | undefined;
+  return function(this:any, ...args: Array<any>) {
+    const context = this;
+    window.clearTimeout(timer);
+    timer = window.setTimeout(function() {
+      fn.apply(context, args);
+      timer = undefined;
+    }, delay);
+  };
+}
+
+/**
+ * 节流函数
+ * @param fn 执行函数
+ * @param delay 延时（ms）
+ */
+export function throttle(fn: Function, delay: number = 500) {
+  let currentTime = Date.now();
+  return function (this:any, ...args: Array<any>) {
+    const context = this;
+    const nowTime = Date.now();
+    console.log(nowTime);
+    if (nowTime - currentTime > delay) {
+      fn.apply(context, args);
+      currentTime = Date.now();
+    }
+  }
+}
