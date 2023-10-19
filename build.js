f6c82cd35a3f2c787dcd0d4f3096beb1f17cf074
LAMMU <chen_yongchao@126.com>
2023-10-19 15:30:20 +0800

    将增删改API配置转函数调用核心代码抽离出去

--- build.js
+++ build.js
@@ -19,7 +19,5 @@ module.exports = {
         openUrl: 'https://lowcode-engine.cn/demo/index.html?debug',
       },
     ],
-    /** 编译commonjs模块给设计器用的, 编译es模块给生产项目用的, 只需要导出generateRemoteHandleMap, 这里做处理了 */
-    "./build.plugin.js"
   ]
 };
--- src/index.tsx
+++ src/index.tsx
@@ -78,9 +78,3 @@ plugin.meta = {
 };
 
 export default plugin;
-
-/** 供主项目调用, 初始化this.remoteHandleMap */
-export { generateRemoteHandleMap } from './utils/data-helper';
-
-/** 内部封装的fetch方法, 谁爱用谁用 */
-export { get, post, request } from './utils/request';
