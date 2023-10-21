ea34b92725f266c562ac6aa2f14a5e3781adc4fb
LAMMU <chen_yongchao@126.com>
2023-10-21 20:05:22 +0800

    解决跨域问题!

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -29,7 +29,8 @@ export async function request(
     headers?: any
   } = {
     headers: {
-      'Content-Type':'application/json'
+      /** 将Content-Type设置成application/json会受到同源政策的限制。他会先发送一个option请求嗅探服务器是否具有应答的能力，然后才会发送真正的请求。 */
+      // 'Content-Type':'application/json;'
     }
   }
 ) {
